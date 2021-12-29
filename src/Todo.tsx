6ea9e2bd7c6a53e887a3efe1af7f876fab3c6df5
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-29 09:31:50 +0900

    型定義の効率的な管理 Pick<T, Key | key>: 指定したkeyの型定義を抜き出す Omit<T, key>: 指定したkeyの型定義を除外する

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,11 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  // Pick<T, Key | key>: 指定したkeyの型定義を抜き出す
+  // props: Pick<TodoType, "userId" | "title" | "completed">
+  // Omit<T, key>: 指定したkeyの型定義を除外する
+  props: Omit<TodoType, "id">
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
