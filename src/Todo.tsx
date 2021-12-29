24e2702d2f565115a6530d7599a8562ccf40deda
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-29 09:14:00 +0900

    取得データに型を定義

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,4 @@
 export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p>{`${title}(ユーザー:${userid})`}</p>;
+  const { title, userId } = props;
+  return <p>{`${title}(ユーザー:${userId})`}</p>;
 };
